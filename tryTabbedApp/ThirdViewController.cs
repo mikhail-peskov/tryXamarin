392b98c06e41e8c6f9ebfcefebd4ff4acdf33364
mikhail-peskov <admin@Mac-Admin.local>
2017-03-01 00:48:40 +0300

    -

---
+++ tryTabbedApp/ThirdViewController.cs
@@ -0,0 +1,18 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace tryTabbedApp
+{
+    public partial class ThirdViewController : UIViewController
+    {
+        public ThirdViewController (IntPtr handle) : base (handle)
+        {
+		}	
+
+		partial void UIButton70_TouchUpInside(UIButton sender)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ tryTabbedApp/ThirdViewController.designer.cs
@@ -0,0 +1,25 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace tryTabbedApp
+{
+    [Register ("ThirdViewController")]
+    partial class ThirdViewController
+    {
+        [Action ("UIButton70_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UIButton70_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+        }
+    }
+}
