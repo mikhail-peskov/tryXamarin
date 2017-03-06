ad1f5e8deb94bffea9311069a1994fb307a6dd5f
mikhail-peskov <admin@Mac-Admin.local>
2017-03-07 02:00:04 +0300

    -

--- Calculator/ViewController.cs
+++ Calculator/ViewController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 using UIKit;
+using CoreGraphics;
 
 namespace Calculator
 {
@@ -14,7 +15,33 @@ namespace Calculator
 		public override void ViewDidLoad()
 		{
 			base.ViewDidLoad();
-			// Perform any additional setup after loading the view, typically from a nib.
+			View.BackgroundColor = UIColor.Yellow;
+
+			var totalAmount = new UITextField(
+				new CGRect(28, 20, View.Bounds.Width - 40, 35))
+			{
+				KeyboardType = UIKeyboardType.DecimalPad,
+				BorderStyle = UITextBorderStyle.RoundedRect,
+				Placeholder = "Enter a total amount"
+			};
+
+			var button = new UIButton(UIButtonType.Custom)
+			{
+				Frame = new CGRect(28, 71, View.Bounds.Width - 40, 45),
+				BackgroundColor = UIColor.FromRGB(0, 0.5f, 0)								                         
+			};
+			button.SetTitle("Calculate", UIControlState.Normal);
+
+			var label = new UILabel(new CGRect(28, 121, View.Bounds.Width, 40))
+			{
+				TextColor = UIColor.Blue,
+				TextAlignment = UITextAlignment.Center,
+				Font = UIFont.SystemFontOfSize(24),
+				Text = "Tip is 0.0$"
+			};
+
+			View.AddSubviews(totalAmount, button, label);
+
 		}
 
 		public override void DidReceiveMemoryWarning()
